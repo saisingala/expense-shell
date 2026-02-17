d8b2d9efec12cf5bf7fa704e815e64688f0b7390
saisingala <saisingala@gmail.com>
2026-02-17 17:57:14 +0530

    MySql Shell Scirpt

--- backend.sh
+++ backend.sh
@@ -65,7 +65,7 @@ unzip /tmp/backend.zip &>> LOG_FILE
 VALIDATE $? "Unzip the downloaded code"
 
 npm install &>> LOG_FILE
-cp /home/ec2-user/expense-shell/backend.service /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell/backend.service.sh /etc/nginx/default.d/expense.conf
 
 #load the database before backend servicess
 
